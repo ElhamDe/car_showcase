beed750dd06983875889aa7136763a277b7e470e
Elham Dermaku <dermaku12@gmail.com>
2023-11-18 23:48:05 +0100

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Login from '@/components/Login';
 import Hero from '../components/Hero';
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <Login />
     </main>
   )
 }
