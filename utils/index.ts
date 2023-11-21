bce68ca6c18c3756f15c111a0cba80424fba2397
Elham Dermaku <dermaku12@gmail.com>
2023-11-21 23:45:04 +0100

    server-side rendering

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,14 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel} = filters;
+
   const headers = {
     'X-RapidAPI-Key': 'eeae8cfecbmsh27690540ff6711ap1b3d73jsn31fd1c690f04',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
   }
 
-  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
+  const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
     headers: headers,
   });
 
