30f3b7571e6098e46a8e558e95ac76384d5a65bc
Elham Dermaku <dermaku12@gmail.com>
2023-11-22 12:27:36 +0100

    change code from server-side to client-side fix a bug

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -5,17 +5,14 @@ import { useRouter } from "next/navigation";
 import { CustomButton } from ".";
 import { updateSearchParams } from "@/utils";
 
-const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {
   const router = useRouter();
 
   const handleNavigation = () => {
     // Calculate the new limit based on the page number and navigation type
     const newLimit = (pageNumber + 1) * 10;
 
-    // Update the "limit" search parameter in the URL with the new value
-    const newPathname = updateSearchParams("limit", `${newLimit}`);
-    
-    router.push(newPathname);
+    setLimit(newLimit)
   };
 
   return (
